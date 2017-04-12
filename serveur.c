b25b852d936260da87aa89aab34a3d3bd6651fe2
Baptiste <baptiste.cauchard@insa-lyon.fr>
2017-04-12 17:51:05 +0200

    J'ai niké ta mère || Le client recoit pas

--- serveur.c
+++ serveur.c
@@ -32,12 +32,17 @@ int main (int argc, char *argv[]) {
     int reutilisation = 1;
     char newPort[4];
     int fils;
+    FILE *file;
+    char tmp;
+    int numACK=0;
+    char tabACK[6];
 
     /*
     * Declaration des structures
     */
-    struct sockaddr_in adresse, adresseClient;
+    struct sockaddr_in adresse, adresseClient, adressePrive;
     socklen_t alen= sizeof(adresseClient);
+    socklen_t alenPrive = sizeof(adressePrive);
 
 
 
@@ -47,6 +52,15 @@ int main (int argc, char *argv[]) {
     */
     int descripteurSocketPublic = socket(AF_INET, SOCK_DGRAM, 0);
     if (descripteurSocketPublic < 0) {
+		perror("ERREUR cannot create socket\n");
+		return -1;
+	}
+
+    /*
+    * Creation du socket pour les données
+    */
+    int descripteurSocketDonnees = socket(AF_INET, SOCK_DGRAM, 0);
+    if (descripteurSocketDonnees < 0) {
 		perror("ERREUR cannot create socket\n");
 		return -1;
 	}
@@ -64,17 +78,35 @@ int main (int argc, char *argv[]) {
 		return -1;
 	}
 
+    //Reutilisation du socket données
+    setsockopt(descripteurSocketDonnees, SOL_SOCKET, SO_REUSEADDR, &reutilisation, sizeof(int));
+
+    adressePrive.sin_family= AF_INET;
+    adressePrive.sin_port= htons(portPrive);
+    adressePrive.sin_addr.s_addr= htonl(INADDR_ANY);
+
+    if (bind(descripteurSocketDonnees, (struct sockaddr*) &adressePrive, sizeof(adressePrive)) == -1) {
+        perror("Bind fail\n");
+        close(descripteurSocketDonnees);
+        return -1;
+    }
+
+
 
 
     /*
     * Reception du SYN
     */
+    int reception;
+    int envoi;
+    char bufferLecture[BUFFSIZE-6];
     while(1) {
         memset(bufferACK, 0, RCVSIZE);
         //Reception du SYN
-        recvfrom(descripteurSocketPublic, bufferACK, RCVSIZE, 0, (struct sockaddr*)&adresseClient, &alen);
+        printf("Receiving SYN\n");
+        reception = recvfrom(descripteurSocketPublic, bufferACK, RCVSIZE, 0, (struct sockaddr*)&adresseClient, &alen);
+        printf("Recu : %d\n", reception);
         if(strncmp(bufferACK,"SYN",3)==0){
-            portPrive++;
             printf("On à reçu le SYN de la part du client ! On determine le nouveau port : %d\n", portPrive);
             memset(bufferEnvoi, 0, BUFFSIZE);
             strcpy(bufferEnvoi, "SYN-ACK");
@@ -84,19 +116,74 @@ int main (int argc, char *argv[]) {
 
             //Envoi du SYN-ACK avec le nouveau port
             sendto(descripteurSocketPublic, bufferEnvoi, sizeof(bufferEnvoi), 0, (struct sockaddr*)&adresseClient, alen);
-
+            portPrive++;
             //printf("Adresse : %s\n", inet_ntoa(adresseClient.sin_addr));
             //printf("Port : %d\n", adresseClient.sin_port);
 
 
             //Reception de l'ACK
             memset(bufferACK, 0, RCVSIZE);
-            recvfrom(descripteurSocketPublic, bufferACK, RCVSIZE, 0, (struct sockaddr*)&adresseClient, &alen);
+            printf("Receiving ACK\n");
+            reception = recvfrom(descripteurSocketPublic, bufferACK, RCVSIZE, 0, (struct sockaddr*)&adresseClient, &alen);
+            printf("Recu 2 : %d\n", reception);
             if(strncmp(bufferACK,"ACK",3)==0){
                 printf("On a bien reçu l'ACK du client !\nOn va fork !\n");
                 fils = fork();
                 if(fils == 0) {
-                    printf("Je suis le fils !\n");
+                    /*printf("Je suis le fils !\n");
+                    close(descripteurSocketPublic);
+                    printf("On attend le nom du fichier ! \n");
+                    memset(bufferACK, 0, RCVSIZE);*/
+                    //recvfrom(descripteurSocketDonnees, bufferACK, RCVSIZE, 0, (struct sockaddr*)&adressePrive, &alenPrive);
+                    //ATTENTION ICI ON RECOIT PAS VRMNT LE FICHIER
+                    printf("Fichier recherché : %s\n", bufferACK);
+                    file = fopen("test.pdf","r");
+    				if(file==NULL){
+    					printf("Erreur d'ouverture du fichier\n");
+    					exit(1);
+    				}
+                    printf("Ouverture réussie !!\n");
+
+                    //Lecture du fichier
+                    numACK++;
+                    numACK =15555;
+                    memset(tabACK, 0, 6);
+                    memset(bufferEnvoi, '0', BUFFSIZE);
+                    memset(bufferLecture, 0, BUFFSIZE-6);
+                    sprintf(tabACK, "%d", numACK);
+
+                    if(numACK<10){
+                        memcpy(&bufferEnvoi[5], tabACK, 1);
+                    }else if(numACK<100){
+                        memcpy(&bufferEnvoi[4], tabACK, 2);
+                    }else if(numACK<1000){
+                        memcpy(&bufferEnvoi[3], tabACK, 3);
+                    }else if(numACK<10000){
+                        memcpy(&bufferEnvoi[2], tabACK, 4);
+                    }else if(numACK<100000){
+                        memcpy(&bufferEnvoi[1], tabACK, 5);
+                    }else if(numACK<1000000){
+                        memcpy(&bufferEnvoi[0], tabACK, 6);
+                    }
+                    printf("Voyons voir : \n");
+                    for(int i =0; i<6; i++) {
+                        printf("%c\n",bufferEnvoi[i]);
+                    }
+                    printf("Test du tableau ACK : %s\n", bufferEnvoi);
+                    for(int i=0; i<BUFFSIZE-6; i++){
+                        tmp = fgetc(file);
+                        if(tmp != EOF) {
+                            bufferLecture[i] = tmp;
+                        } else {
+                            printf("%d\n", tmp);
+                        }
+                    }
+                    printf("%s\n", bufferLecture);
+                    memcpy(&bufferEnvoi[6], bufferLecture, BUFFSIZE-6);
+                    printf("Test après ajout ACK : %s\n", bufferEnvoi);
+                    envoi = sendto(descripteurSocketDonnees, bufferEnvoi, sizeof(bufferEnvoi), 0, (struct sockaddr*)&adressePrive, alenPrive);
+                    printf("Envoie : %d\nSur le port :%d\n", envoi, htons(adressePrive.sin_port));
+                    exit(1);
                 } else{
                 }
             }
