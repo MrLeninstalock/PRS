e064ded0918aed6a26b39f25c6fd16c9928bc762
Baptiste <baptiste.cauchard@insa-lyon.fr>
2017-05-03 15:01:18 +0200

    Sauvegarde 1 ca marche

--- serveur.c
+++ serveur.c
@@ -37,6 +37,7 @@ int main (int argc, char *argv[]) {
     int numACK=0;
     char tabACK[6];
 
+
     /*
     * Declaration des structures
     */
@@ -116,7 +117,7 @@ int main (int argc, char *argv[]) {
 
             //Envoi du SYN-ACK avec le nouveau port
             sendto(descripteurSocketPublic, bufferEnvoi, sizeof(bufferEnvoi), 0, (struct sockaddr*)&adresseClient, alen);
-            portPrive++;
+            //portPrive++;
             //printf("Adresse : %s\n", inet_ntoa(adresseClient.sin_addr));
             //printf("Port : %d\n", adresseClient.sin_port);
 
@@ -130,14 +131,15 @@ int main (int argc, char *argv[]) {
                 printf("On a bien reçu l'ACK du client !\nOn va fork !\n");
                 fils = fork();
                 if(fils == 0) {
-                    /*printf("Je suis le fils !\n");
-                    close(descripteurSocketPublic);
                     printf("On attend le nom du fichier ! \n");
-                    memset(bufferACK, 0, RCVSIZE);*/
-                    //recvfrom(descripteurSocketDonnees, bufferACK, RCVSIZE, 0, (struct sockaddr*)&adressePrive, &alenPrive);
-                    //ATTENTION ICI ON RECOIT PAS VRMNT LE FICHIER
+                    memset(bufferACK, 0, RCVSIZE);
+                    recvfrom(descripteurSocketDonnees, bufferACK, RCVSIZE, 0, (struct sockaddr*)&adressePrive, &alenPrive);
+                    close(descripteurSocketPublic);
+                    numACK =0;
+                    int onContinue = 1;
+                    int i;
                     printf("Fichier recherché : %s\n", bufferACK);
-                    file = fopen("test.pdf","r");
+                    file = fopen(bufferACK,"r");
     				if(file==NULL){
     					printf("Erreur d'ouverture du fichier\n");
     					exit(1);
@@ -145,44 +147,60 @@ int main (int argc, char *argv[]) {
                     printf("Ouverture réussie !!\n");
 
                     //Lecture du fichier
-                    numACK++;
-                    numACK =15555;
-                    memset(tabACK, 0, 6);
-                    memset(bufferEnvoi, '0', BUFFSIZE);
-                    memset(bufferLecture, 0, BUFFSIZE-6);
-                    sprintf(tabACK, "%d", numACK);
+                    while(onContinue == 1) {
+                        numACK++;
 
-                    if(numACK<10){
-                        memcpy(&bufferEnvoi[5], tabACK, 1);
-                    }else if(numACK<100){
-                        memcpy(&bufferEnvoi[4], tabACK, 2);
-                    }else if(numACK<1000){
-                        memcpy(&bufferEnvoi[3], tabACK, 3);
-                    }else if(numACK<10000){
-                        memcpy(&bufferEnvoi[2], tabACK, 4);
-                    }else if(numACK<100000){
-                        memcpy(&bufferEnvoi[1], tabACK, 5);
-                    }else if(numACK<1000000){
-                        memcpy(&bufferEnvoi[0], tabACK, 6);
-                    }
-                    printf("Voyons voir : \n");
-                    for(int i =0; i<6; i++) {
-                        printf("%c\n",bufferEnvoi[i]);
-                    }
-                    printf("Test du tableau ACK : %s\n", bufferEnvoi);
-                    for(int i=0; i<BUFFSIZE-6; i++){
-                        tmp = fgetc(file);
-                        if(tmp != EOF) {
-                            bufferLecture[i] = tmp;
-                        } else {
-                            printf("%d\n", tmp);
+                        memset(tabACK, 0, 6);
+                        memset(bufferEnvoi, '0', BUFFSIZE);
+                        memset(bufferLecture, 0, BUFFSIZE-6);
+                        sprintf(tabACK, "%d", numACK);
+
+                        if(numACK<10){
+                            memcpy(&bufferEnvoi[5], tabACK, 1);
+                        }else if(numACK<100){
+                            memcpy(&bufferEnvoi[4], tabACK, 2);
+                        }else if(numACK<1000){
+                            memcpy(&bufferEnvoi[3], tabACK, 3);
+                        }else if(numACK<10000){
+                            memcpy(&bufferEnvoi[2], tabACK, 4);
+                        }else if(numACK<100000){
+                            memcpy(&bufferEnvoi[1], tabACK, 5);
+                        }else if(numACK<1000000){
+                            memcpy(&bufferEnvoi[0], tabACK, 6);
                         }
+                        printf("Voyons voir : \n");
+                        for(int i =0; i<6; i++) {
+                            printf("%c",bufferEnvoi[i]);
+                        }
+                        printf("\n");
+                        //printf("Test du tableau ACK : %s\n", bufferEnvoi);
+                        for(i=0; i<BUFFSIZE-6; i++){
+                            tmp = fgetc(file);
+                            if(tmp != EOF && onContinue==1) {
+                                bufferLecture[i] = tmp;
+                            } else {
+                                bufferLecture[i] = tmp;
+                                printf("On est arrivés a la fin !\n");
+                                onContinue = 0;
+                                break;
+                            }
+                        }
+                        //printf("%s\n", bufferLecture);
+                        memcpy(&bufferEnvoi[6], bufferLecture, BUFFSIZE-6);
+
+                        //printf("Test après ajout ACK : %s\n", bufferEnvoi);
+                        envoi = sendto(descripteurSocketDonnees, bufferEnvoi, i+6, 0, (struct sockaddr*)&adressePrive, alenPrive);
+
+
+                        printf("Envoie : %d\nSur le port :%d & l'adresse %s\n", envoi, htons(adressePrive.sin_port), inet_ntoa(adressePrive.sin_addr));
                     }
-                    printf("%s\n", bufferLecture);
-                    memcpy(&bufferEnvoi[6], bufferLecture, BUFFSIZE-6);
-                    printf("Test après ajout ACK : %s\n", bufferEnvoi);
-                    envoi = sendto(descripteurSocketDonnees, bufferEnvoi, sizeof(bufferEnvoi), 0, (struct sockaddr*)&adressePrive, alenPrive);
-                    printf("Envoie : %d\nSur le port :%d\n", envoi, htons(adressePrive.sin_port));
+                    memset(bufferEnvoi, '0', BUFFSIZE);
+                    bufferEnvoi[0] = 'F';
+                    bufferEnvoi[1] = 'I';
+                    bufferEnvoi[2] = 'N';
+                    envoi = sendto(descripteurSocketDonnees, "FIN", sizeof("FIN"), 0, (struct sockaddr*)&adressePrive, alenPrive);
+
+                    //printf("%s\n", bufferEnvoi);
                     exit(1);
                 } else{
                 }
